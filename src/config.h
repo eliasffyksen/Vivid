1ba4b40fcaaf45a75de3fe67811607a901ae11f6
Ole Strohm <strohm99@gmail.com>
2017-08-31 14:15:02 +0200

    YAY
    tok meg bare 1 time og 24

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 #define CONFIG_H
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define DEBUG true
 #define PLATFORM WIN64
 
---
+++ src/graphics/shader.h
@@ -0,0 +1,32 @@
+//
+// Created by Ole on 29.08.2017.
+//
+
+#ifndef GLTEST_SHADER_H
+#define GLTEST_SHADER_H
+
+#include <iostream>
+#include "GL/glew.h"
+
+namespace engine {
+	namespace graphics {
+
+		class Shader {
+		public:
+			static GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
+
+		private:
+			GLuint programID;
+		public:
+			Shader(std::string path);
+
+			~Shader();
+
+			void bind() const;
+		};
+
+	}
+}
+
+
+#endif //GLTEST_SHADER_H
