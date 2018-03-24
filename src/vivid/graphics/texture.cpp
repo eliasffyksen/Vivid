c72494527e850dc574b5a13224c92baaa0f21ab0
ole strohm <Strohm99@gmail.com>
2018-03-24 16:27:06 +0100

    Textures are messed up...

--- src/vivid/graphics/texture.cpp
+++ src/vivid/graphics/texture.cpp
@@ -21,11 +21,12 @@ namespace vivid { namespace graphics {
 		glBindTexture(GL_TEXTURE_2D, textureID);
 		
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (const void*) image.getPixels());
+		LOG(width << "x" << height);
 		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 		
 		glGenerateMipmap(GL_TEXTURE_2D);
 		
