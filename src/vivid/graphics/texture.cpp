af88505dbad1d7af20e706f08f44b721e6820f97
ole strohm <Strohm99@gmail.com>
2018-03-24 16:27:28 +0100

    Revert "Merge branch 'input'"
    This reverts commit 492b9a4fd757781030687db0cc004bbc58a78ffc.

--- src/vivid/graphics/texture.cpp
+++ src/vivid/graphics/texture.cpp
@@ -21,12 +21,11 @@ namespace vivid { namespace graphics {
 		glBindTexture(GL_TEXTURE_2D, textureID);
 		
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (const void*) image.getPixels());
-		LOG(width << "x" << height);
 		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		
 		glGenerateMipmap(GL_TEXTURE_2D);
 		
